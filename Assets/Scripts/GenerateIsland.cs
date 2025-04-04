2927bb33c2512a427eb3ffeb284711734011d363
Alex <humphriesa02@gmail.com>
2025-04-04 15:35:22 -0700

    Some prefabs for generation

--- Assets/Scripts/GenerateIsland.cs
+++ Assets/Scripts/GenerateIsland.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class GenerateIsland : MonoBehaviour
 {
-    [SerializeField] private GameObject[] possibleObjects;
-    [SerializeField] private GameObject[] spawnLocations;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject[] possibleObjects; // Objects that can be spawned on an island
+    [SerializeField] private GameObject[] spawnLocations; // Locations to spawn the objects on the island
+
     void Start()
     {
 		for (int i = 0; i < spawnLocations.Length; i++)
@@ -15,10 +15,8 @@ public class GenerateIsland : MonoBehaviour
 				GameObject randomObject = possibleObjects[Random.Range(0, possibleObjects.Length)];
 
 				// Instantiate the object as a child of the spawn location
-				GameObject spawnedObject = Instantiate(randomObject, spawnLocations[i].transform.position, Quaternion.identity);
-
-				// Set the parent to maintain hierarchy
-				spawnedObject.transform.SetParent(spawnLocations[i].transform);
+				GameObject tempObj = Instantiate(randomObject, spawnLocations[i].transform.position, Quaternion.identity, spawnLocations[i].transform);
+				tempObj.transform.Rotate(0, Random.Range(0, 360), 0);
 			}
 		}
     }
